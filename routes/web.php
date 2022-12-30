e164a73175c6923c746df640b03e4db926d43ffc
YCBEN <yacinebendou@gmail.com>
2022-12-30 15:39:32 +0100

    admin middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
+
+    Route::get('/',[AdminController::class, 'index'])->name('index');
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,5 +28,14 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
 
 require __DIR__.'/auth.php';
